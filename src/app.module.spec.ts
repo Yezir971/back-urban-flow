1f87469130d47d46e7191839291bf226854f862a
Yezir971 <james_ahmedaly@yahoo.com>
2026-07-19 13:51:34 +0200

    feat: refactor environment variable setup in AppModule tests

--- src/app.module.spec.ts
+++ src/app.module.spec.ts
@@ -1,14 +1,11 @@
+process.env.SUPABASE_URL = 'https://example.supabase.co';
+process.env.SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4YW1wbGUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDAwMDAwMCwiZXhwIjoyNTIwMDAwMDAwfQ.dummy_signature';
+process.env.OTP_URL = 'https://example.com';
+
 import { Test } from '@nestjs/testing';
 import { AppModule } from './app.module';
 
 describe('AppModule', () => {
-  beforeAll(() => {
-    process.env.SUPABASE_URL = 'https://example.supabase.co';
-    process.env.SUPABASE_KEY =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV4YW1wbGUiLCJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMDAwMDAwMCwiZXhwIjoyNTIwMDAwMDAwfQ.dummy_signature';
-    process.env.OTP_URL = 'https://example.com';
-  });
-
   it('should compile the module', async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
